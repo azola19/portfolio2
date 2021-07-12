25882c09918f7a7ea5803b1214415e029fbb2f3e
Azola <77683397+azola19@users.noreply.github.com>
2021-07-12 16:50:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD4yEwDXcMoPUs2ujVB-YW8REWGdqW8O4w",
+    authDomain: "azolamafilika-portfolio.firebaseapp.com",
+    projectId: "azolamafilika-portfolio",
+    storageBucket: "azolamafilika-portfolio.appspot.com",
+    messagingSenderId: "1024947960049",
+    appId: "1:1024947960049:web:5847cfffdfbd0e7f301150",
+    measurementId: "G-M583GNHJP8"
+  };
